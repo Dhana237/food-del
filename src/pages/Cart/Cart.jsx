484839d7457c3616c3bb1b1ba23db4a6f665092b
Dhana237 <dhanalakshmi763927@gmail.com>
2025-07-27 20:11:33 +0530

    First commit

---
+++ src/pages/Cart/Cart.jsx
@@ -0,0 +1,92 @@
+import React, { useContext } from 'react'
+import './Cart.css'
+import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
+const Cart = () => {
+  const navigate= useNavigate()
+  const{cartItems,food_list,removeFromCart,getTotalCartAmount,url,token}= useContext(StoreContext)
+
+  const handleProceed = () => {
+      if (token && getTotalCartAmount()!==0) {
+        navigate('/order')
+      }
+      else if(!token && getTotalCartAmount()===0){
+         toast.error("Login required. Add products to cart to get started.")
+      }
+      else if(!token){
+           toast.error("Please log in to proceed.")
+      }
+      else if(getTotalCartAmount()===0){
+        toast.error("Add products to cart")
+      }
+  }
+
+  return (
+    <div className='cart'>
+      <div className="cart-items">
+     <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+      </div>
+      <br />
+      <hr />
+      {food_list.map((item,index)=>{
+        if(cartItems[item._id]>0){
+          return (
+            <div>
+            <div className='cart-items-title cart-items-item'>
+              <img src={url+"/images/"+item.image} alt="" />
+              <p>{item.name}</p>
+              <p>${item.price}</p>
+              <p>{cartItems[item._id]}</p>
+              <p>${item.price*cartItems[item._id]}</p>
+              <p className='cross' onClick={(re)=>removeFromCart(item._id )}>x</p>
+            </div>
+            <hr />
+            </div>
+          )
+        }
+      })}
+      </div>
+<div className="cart-bottom">
+  <div className="cart-total">
+    <h2>Cart Totals</h2>
+    <div>
+      <div className="cart-total-details">
+        <p>Subtotal</p>
+      <p>${getTotalCartAmount()}</p>
+      </div>
+      <hr />
+      <div className="cart-total-details">
+        <p>Delivery fee</p>
+      <p>${getTotalCartAmount()===0?0:2}</p>
+      </div>
+      <hr />
+      <div className="cart-total-details">
+        <b>Total</b>
+        <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+      </div>
+    </div>
+      <button onClick={handleProceed}>PROCEED TO CHECKOUT</button>
+  </div>
+  <div className="cart-promocode">
+    <div>
+      <p>If you have promo code, Enter it here</p>
+      <div className="cart-promocode-input">
+        <input type="text"placeholder='promo code' />
+        <button>Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Cart
